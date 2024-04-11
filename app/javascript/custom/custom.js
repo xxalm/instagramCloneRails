2846ccb9125199b339cd6fe786d42c7e666d95e0
xxalm <leckalmeida@gmail.com>
2024-04-11 15:55:10 -0300

    Add preview and filepond

---
+++ app/javascript/custom/custom.js
@@ -0,0 +1,7 @@
+import * as FilePond from 'filepond';
+
+// Get a reference to the file input element
+const inputElement = document.querySelector("#post-images");
+
+// Create a FilePond instance
+const pond = FilePond.create(inputElement);
